d90df1a21c386b57188ee684ca5009297d17be1d
present0329 <guswo0329@gmail.com>
2023-09-27 18:06:17 +0900

    카드식 글 표시 완

--- src/Components/FreePost.js
+++ src/Components/FreePost.js
@@ -5,9 +5,12 @@ function FreePost(){
         <Table striped bordered hover>
           <thead>
             <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Actions</th>
+              <th>번호</th>
+              <th>말머리</th>
+              <th>제목</th>
+              <th>글쓴이</th>
+              <th>작성일</th>
+              <th>조회수</th>
             </tr>
           </thead>
           <tbody>
@@ -15,10 +18,11 @@ function FreePost(){
                 <td>post.id</td>
                 <td>post.title</td>
                 <td>
-                  <Button variant="danger" >
-                    Delete
-                  </Button>
+                  o
                 </td>
+                <td>o</td>
+                <td>o</td>
+                <td>o</td>
               </tr>
           </tbody>
         </Table>
--- src/Pages/PageOne.js
+++ src/Pages/PageOne.js
@@ -1,12 +1,39 @@
-import CardPost from "../Components/CardPost";
-
 //카드식게시판
+import axios from "axios";
+import CardPost from "../Components/CardPost";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const CardsDiv=styled.div`
+
+display: flex;
+flex-wrap: wrap;
+width:80%;
+margin:10%;
+margin-top:5%;
+margin-bottom:5%;
+border: solid;
+`
 function PageOne()
 {
+    const [cards,setCards]=useState([]);
+
+    useEffect(()=>{
+    const test=axios.get(`http://localhost:3001/data`)
+    .then((response)=>{
+        setCards(response.data)
+    })
+    .catch((error)=>console.log(error))
+
+},[]);
     return(
-        <div>
-            <CardPost/>
-        </div>
+        <CardsDiv>
+            {cards.map(cards=>{
+                return (<CardPost data={cards}/>)
+            })
+            }
+            
+        </CardsDiv>
     )
 }
 export default PageOne;
