302a9789b4b72285e14664599070085289021953
present0329 <guswo0329@gmail.com>
2023-10-03 17:35:06 +0900

    opensea nft api 적용

--- src/Pages/TestPage.js
+++ src/Pages/TestPage.js
@@ -1,7 +1,52 @@
+import axios from "axios";
+import { useState,useEffect } from "react";
+import NftCard from "../Components/NftCard";
 function TestPage()
 {
+
+    const [cards,setCards]=useState([]);
+    
+    useEffect(()=>{
+        axios.get('https://api.opensea.io/v2/collection/deadmigos-official/nfts?limit=3',{
+            headers:{
+                accept: 'application/json', 
+                'X-API-KEY': process.env.REACT_APP_OPENSEA_API_KEY
+            }
+        })
+        
+    .then((response)=>{
+        setCards(response.data.nfts)
+        //console.log(response.data.nfts)
+    })
+    .catch((error)=>console.log(error))
+
+},[]);
+
+
+
+// axios.get('https://api.opensea.io/v2/collection/deadmigos-official/nfts?limit=3',{
+//     headers:{
+//         accept: 'application/json', 
+//         'X-API-KEY': process.env.REACT_APP_OPENSEA_API_KEY
+//     }
+// })
+
+//   .then(function (response) {
+//     console.log(response.data.nfts);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .finally(function () {
+//   });
+
+
     return(
         <>
+            {cards.map(cards=>{
+                return (<NftCard data={cards}/>)
+            })
+            }
         </>
     )
 }
