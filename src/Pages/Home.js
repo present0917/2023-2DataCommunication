5b585bc31e16be37de821a1dbb6ee68e5d8ac6eb
ash9river <ash9river@gmail.com>
2023-10-06 23:05:28 +0900

    네비바 테스트 스타일 적용중

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,10 @@
 import FireInfo from "../Components/FireInfo";
 import ControlledCarousel from "../Components/ControlledCarousel";
-import FireInfo from "../Components/FireInfo";
 
 function Home()
 {
     return(
         <div>
-            <FireInfo/>
         <ControlledCarousel/>                
             <FireInfo/>
         </div>
